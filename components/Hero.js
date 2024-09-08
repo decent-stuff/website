142b5e8ba0c606a5440218c9f751143ae3345953
Yan Liu <yanliu8@proton.me>
2024-09-08 20:37:14 +0200

    Add a link to GitHub discussions

--- components/Hero.js
+++ components/Hero.js
@@ -18,10 +18,10 @@ const Hero = () => {
               without a vendor lock-in
             </p>
             <Link
-              href="#contact"
+              href="https://github.com/orgs/decent-cloud/discussions"
               className="bg-white text-blue-600 py-2 px-6 rounded-full text-lg font-semibold hover:bg-gray-100 transition duration-300"
             >
-              Get Notified
+              Get involved
             </Link>
           </div>
         </section>
