541c0b10f88352d1c8e83977da650f28c98a558b
Yan Liu <yanliu8@proton.me>
2024-09-08 20:27:28 +0200

    Statically include tailwindcss and fonts

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+// pages/_app.js
+
+import "../styles/globals.css";
+
+function HomeApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default HomeApp;
--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,6 @@ const Home = () => {
           Can Trust
         </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script src="https://cdn.tailwindcss.com" defer />
-        <link
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-          rel="stylesheet"
-        />
       </Head>
       <Hero />
       <About />
