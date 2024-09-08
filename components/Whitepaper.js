66a7225cfd7d54ed4367a0d8d17469db172a5401
Yan Liu <yanliu8@proton.me>
2024-09-08 21:56:19 +0200

    Cleanup whitepaper component

--- components/Whitepaper.js
+++ components/Whitepaper.js
@@ -1,7 +1,3 @@
-import { Worker, Viewer } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-
 const Whitepaper = () => {
   return (
     <div className="container mx-auto text-center my-8">
