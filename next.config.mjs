619cc02bec8562ed1c60139969baa0aae1430445
Yan Liu <yanliu8@proton.me>
2024-10-29 22:00:13 +0100

    do not optimize images

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true,
   },
   // Uncomment this line if you want to use static export in the future
   output: "export",
