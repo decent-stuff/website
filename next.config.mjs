6ccebacd2a702a9c0a5278f89c9e3ea95ccd5cb7
Yan Liu <yanliu8@proton.me>
2024-10-29 21:49:27 +0100

    Enable export

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
     ],
   },
   // Uncomment this line if you want to use static export in the future
-  // output: "export",
+  output: "export",
 };
 
 export default nextConfig;
