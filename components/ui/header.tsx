419efe43161eed28e40cfa979384bcdfbe0aec3e
Teodor Vidakovic <teodorv22@gmail.com>
2025-01-24 01:01:59 +0100

    feat: update header component

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 interface HeaderSectionProps {
@@ -7,14 +8,20 @@ interface HeaderSectionProps {
 
 const HeaderSection: React.FC<HeaderSectionProps> = ({ title, subtitle }) => {
     return (
-        <div className="text-center mb-12">
+        <motion.div
+            className="text-center mb-12"
+            initial={{opacity: 0, y: 20}}
+            whileInView={{opacity: 1, y: 0}}
+            viewport={{once: true, amount: 0.3}} // Trigger when 30% is in viewport
+            transition={{duration: 0.8, ease: "easeOut"}}
+        >
             <h3 className="text-2xl md:text-3xl font-bold text-center mb-4">
                 {title}
             </h3>
             <p className="text-gray-300 max-w-2xl mx-auto text-lg md:text-xl">
                 {subtitle}
             </p>
-        </div>
+        </motion.div>
     );
 };
 
