96cc1870882e007ede377b8989603c0569e86d0c
Yan Liu <yanliu8@proton.me>
2025-01-28 21:41:30 +0100

    fix: hydration error of div inside p

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -18,9 +18,9 @@ const HeaderSection: React.FC<HeaderSectionProps> = ({ title, subtitle }) => {
             <h3 className="text-2xl md:text-3xl font-bold text-center mb-4">
                 {title}
             </h3>
-            <p className="text-gray-300 max-w-2xl mx-auto text-lg md:text-xl">
+            <div className="text-gray-300 max-w-2xl mx-auto text-lg md:text-xl">
                 {subtitle}
-            </p>
+            </div>
         </motion.div>
     );
 };
